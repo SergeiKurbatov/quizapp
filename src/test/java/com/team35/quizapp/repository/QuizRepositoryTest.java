1640ad9384ad03a920ac287fc2bd22585084af81
andrei.pugatsov <andrei.pugatsov@gmail.com>
2026-03-14 18:09:36 +0200

    (feat)QUIZ-4 : updated import

--- src/test/java/com/team35/quizapp/repository/QuizRepositoryTest.java
+++ src/test/java/com/team35/quizapp/repository/QuizRepositoryTest.java
@@ -5,7 +5,7 @@ import com.team35.quizapp.entity.Question;
 import com.team35.quizapp.entity.Quiz;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @Testcontainers
 class QuizRepositoryTest {
 
