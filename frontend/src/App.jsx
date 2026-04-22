2ff63dafa74a3f4129cff773e64c457a05f2c8b0
Julia Taro <julia.taro@gmail.com>
2026-04-22 14:05:16 +0300

    resolve merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,33 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import MyQuizzes from "./pages/MyQuizzes";
+<<<<<<< HEAD
 import JoinGame from "./pages/JoinGame";
+=======
+import PlayerView from "./pages/Player/PlayerView";
+>>>>>>> QUIZ-40
 
 const router = createBrowserRouter([
   { path: "/Login", element: <Login /> },
   { path: "/Register", element: <Register /> },
-  { 
-    path: "/Home", 
+  {
+    path: "/Home",
     element: (
       <ProtectedRoute>
         <Home />
       </ProtectedRoute>
-    ) 
+    ),
   },
   { path: "/", element: <Login /> },
-  { 
-    path: "/HostCreateGame", 
+  {
+    path: "/HostCreateGame",
     element: (
       <ProtectedRoute>
         <HostCreateGame />
       </ProtectedRoute>
-    ) 
+    ),
   },
+<<<<<<< HEAD
   { 
     path: "/HostLobby/:gamePin",
     element: (
@@ -38,10 +43,22 @@ const router = createBrowserRouter([
   },
   { path: "/MyQuizzes", element: <ProtectedRoute><MyQuizzes /></ProtectedRoute> },
   { path: "/game/:gamePin", element: <JoinGame /> },
+=======
+  { path: "/HostLobby", element: <HostLobby /> },
+  {
+    path: "/MyQuizzes",
+    element: (
+      <ProtectedRoute>
+        <MyQuizzes />
+      </ProtectedRoute>
+    ),
+  },
+  { path: "/Player", element: <PlayerView /> },
+>>>>>>> QUIZ-40
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
-
+console.log(PlayerView);
 export default App;
